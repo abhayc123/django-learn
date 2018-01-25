edf0520aa85480e05ed5fa29c0a3c2af14e20824
abhay <abhay.singh@rootinfosol.com>
2018-01-26 04:29:26 +0530

    add genric foreign key relatonship and add param limit_choices_to

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -4,48 +4,80 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
-class Subject(models.Model):
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200 , unique = True)
+class 	Subject(models.Model):
+		title = models.CharField(max_length=200)
+		slug = models.SlugField(max_length=200 , unique = True)
 
-	class Meta:
-		ordering = ('title',)
-
-
-	def __str__(self):
-		return self.title
-
-class Course(models.Model):
-	owner = models.ForeignKey(User , related_name='courses_created')
-	subject = models.ForeignKey(Subject , related_name='courses')
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200 , unique = True)
-	overview = models.TextField()
-	created = models.DateTimeField(auto_now_add = True)
-
-	class Meta:
-		ordering = ('created',)
-
-	def __str__(self):
-		return self.title
+		class Meta:
+			ordering = ('title',)
 
 
-class module(models.Model):
-	course = models.ForeignKey(Course , related_name = 'modules')
-	title = models.CharField(max_length=200)
-	description = models.TextField(blank = True)
+		def __str__(self):
+			return self.title
+
+class 	Course(models.Model):
+		owner = models.ForeignKey(User , related_name='courses_created')
+		subject = models.ForeignKey(Subject , related_name='courses')
+		title = models.CharField(max_length=200)
+		slug = models.SlugField(max_length=200 , unique = True)
+		overview = models.TextField()
+		created = models.DateTimeField(auto_now_add = True)
+
+		class Meta:
+			ordering = ('created',)
+
+		def __str__(self):
+			return self.title
 
 
-	def __str__(self):
-		return self.title
+class 	module(models.Model):
+		course = models.ForeignKey(Course , related_name = 'modules')
+		title = models.CharField(max_length=200)
+		description = models.TextField(blank = True)
 
 
+		def __str__(self):
+			return self.title
 
-class Content(models.Model):
-	module = models.Foreignkey(module , related_name='contents')
-	content_type = models.Foreignkey(ContentType)
-	object_id = models.PositiveIntegerField()
-	item = GenericForeignKey('content_type','object_id')
+
+#This table will be used for containing content type and content reference infromation of module
+class 	Content(models.Model):
+		module = models.ForeignKey(module , related_name='contents')
+		content_type = models.ForeignKey(ContentType ,limit_choices_to = {'model__in' : ('text','video','image','file')})
+		object_id = models.PositiveIntegerField()
+		item = GenericForeignKey('content_type','object_id')
+
+
+#This table will be used for contaning the content of module
+class 	ItemBase(models.Model):
+		owner = models.ForeignKey(User , related_name='%(class)s_related')
+		title = models.CharField(max_length = 250)
+		created = models.DateTimeField(auto_now_add=True)
+		updated = models.DateTimeField(auto_now = True)
+
+
+		class Meta:
+			abstract = True
+
+		def __str__(self):
+			return self.title
+
+
+class 	Text(ItemBase):
+		content = models.TextField()
+
+
+class 	File(ItemBase):
+		file = models.FileField(upload_to='files')
+
+
+class 	Image(ItemBase):
+		file = models.FileField(upload_to='images')
+
+
+class 	Video(ItemBase):
+		url = models.URLField()
+
 
 
 
